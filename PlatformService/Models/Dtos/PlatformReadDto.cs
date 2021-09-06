3f2adb4cbb36c30707b5ef823951e385c4a3b131
ouzdev <oguzcangencc@hotmail.com>
2021-09-06 23:45:37 +0300

    PlatformReadDto class created

---
+++ PlatformService/Models/Dtos/PlatformReadDto.cs
@@ -0,0 +1,11 @@
+namespace PlatformService.Dtos
+{
+    public class PlatformReadDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Cost { get; set; }
+
+    }
+}
