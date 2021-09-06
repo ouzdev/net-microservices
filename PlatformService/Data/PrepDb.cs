3e1bd6638c2c6707c8798a93315d3f29e6d978a0
ouzdev <oguzcangencc@hotmail.com>
2021-09-06 21:14:53 +0300

    SeedData method is coded in PrepDb class

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,10 +1,22 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PlatformService.Data
 {
-    public static class PrepDb{
-        public static void PrepPopulation(IApplicationBuilder app){
-            
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+
         }
     }
 }
