d7d4f19260370f38a48e5a5c20ce320cf8321ced
ouzdev <oguzcangencc@hotmail.com>
2021-09-06 21:43:45 +0300

    SeedData method coded

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
 
 namespace PlatformService.Data
 {
@@ -16,7 +19,21 @@ namespace PlatformService.Data
 
         private static void SeedData(AppDbContext context)
         {
+            if (!context.Platforms.Any())
+            {
+                    Console.WriteLine("---> Seeding Data");
 
+                    context.Platforms.AddRange(
+                        new Platform(){Name="Xbox", Publisher="Microsoft",Cost="Free"},
+                        new Platform(){Name="Docker", Publisher="Docker Inc.",Cost="Free"},
+                        new Platform(){Name="Kubernetes", Publisher="Cloud Native Computing Foundation",Cost="Free"}
+                    );
+                    context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine("---> We already have data");
+            }
         }
     }
 }
