fb4566ddcd1d30e94634449de9977e8e10966c47
ouzdev <oguzcangencc@hotmail.com>
2021-10-01 22:39:32 +0300

    Containers are deployed to kubernetes.

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/c/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
