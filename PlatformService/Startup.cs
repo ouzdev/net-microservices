e9a257cc8d9860e2fbbd138d6440bf6fc9a62e35
ouzdev <oguzcangencc@hotmail.com>
2021-10-24 23:01:13 +0300

    Editing Startup.cs file

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -14,16 +14,28 @@ namespace PlatformService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
-
-        public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemoryDb"));
+            if (_env.IsProduction())
+            {
+                Console.WriteLine("----> Using SQL Server Database");
+                services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+
+            }
+            else
+            {
+                Console.WriteLine("----> Using InMemoryDb");
+                services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemoryDb"));
+            }
+
             services.AddScoped<IPlatformRepo, PlatformRepo>();
 
             services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
