41fc899aec8e9976fc3e3a39e2815fffd3c0300a
ouzdev <oguzcangencc@hotmail.com>
2021-10-24 23:11:45 +0300

    Created migration

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using PlatformService.Models;
 
@@ -8,27 +10,41 @@ namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>(), isProd);
             }
 
         }
 
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool isProd)
         {
+            if (isProd)
+            {
+                Console.WriteLine("---> Attemting to apply migrations...");
+                try
+                {
+                    context.Database.Migrate();
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine($"---> Could not run migrations: {ex.Message}");
+                }
+            }
             if (!context.Platforms.Any())
             {
-                    Console.WriteLine("---> Seeding Data");
+                Console.WriteLine("---> Seeding Data");
 
-                    context.Platforms.AddRange(
-                        new Platform(){Name="Xbox", Publisher="Microsoft",Cost="Free"},
-                        new Platform(){Name="Docker", Publisher="Docker Inc.",Cost="Free"},
-                        new Platform(){Name="Kubernetes", Publisher="Cloud Native Computing Foundation",Cost="Free"}
-                    );
-                    context.SaveChanges();
+                context.Platforms.AddRange(
+                    new Platform() { Name = "Xbox", Publisher = "Microsoft", Cost = "Free" },
+                    new Platform() { Name = "Docker", Publisher = "Docker Inc.", Cost = "Free" },
+                    new Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                );
+                context.SaveChanges();
             }
             else
             {
