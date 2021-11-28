57edbbe16d061748e77edd67693c47e3854594ad
ouzdev <oguzcangencc@hotmail.com>
2021-11-28 17:37:08 +0300

    Added autoMapper services in Startup.cs

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -28,6 +28,7 @@ namespace CommandsService
         {
 
             services.AddControllers();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
