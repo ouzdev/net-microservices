83b930cb99e5d5cc41bc626e35a98c9084d6cbee
ouzdev <oguzcangencc@hotmail.com>
2021-11-28 16:02:46 +0300

    Added data annotations in models files

--- CommandsService/Models/Platform.cs
+++ CommandsService/Models/Platform.cs
@@ -1,12 +1,16 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CommandsService.Models
 {
     public class Platform
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public int ExtarnalID { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public ICollection<Command> Commands { get; set; } = new List<Command>();
