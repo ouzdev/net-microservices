8baa00e3e7b3748b94b17bd55833a62776012af6
ouzdev <oguzcangencc@hotmail.com>
2021-11-28 17:54:38 +0300

    Added AddDbContext services in Startup.cs

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandsService.Data.Concrete;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,10 @@ namespace CommandsService
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<CommandDbContext>(options => 
+                options.UseInMemoryDatabase("InMemoryDb")
+            );
+            
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
