e97d097de13f9e6f94c22fc6bc43bd302281bd61
ouzdev <oguzcangencc@hotmail.com>
2021-11-28 16:13:32 +0300

    Created onModelCreating method in CommandDbContext

--- CommandsService/Data/CommandDbContext.cs
+++ CommandsService/Data/CommandDbContext.cs
@@ -12,5 +12,21 @@ namespace CommandsService.Data
 
         public DbSet<Platform> Platforms {get; set; }
         public DbSet<Command> Commands {get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+                modelBuilder
+                .Entity<Platform>()
+                .HasMany(p=>p.Commands)
+                .WithOne(p=>p.Platform!)
+                .HasForeignKey(p=>p.PlatformId);
+
+                 modelBuilder
+                .Entity<Command>()
+                .HasOne(p=>p.Platform)
+                .WithMany(p=>p.Commands)
+                .HasForeignKey(p=>p.PlatformId);
+        }
     }
 }
