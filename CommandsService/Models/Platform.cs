f3feafaf7ea35dab5f0241c5f151d6dd9c85bd09
ouzdev <oguzcangencc@hotmail.com>
2021-11-28 15:55:42 +0300

    Created platform.cs file in Models folder

---
+++ CommandsService/Models/Platform.cs
@@ -0,0 +1,10 @@
+namespace CommandsService.Models
+{
+    public class Platform
+    {
+        public int Id { get; set; }
+        public int ExtarnalID { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
