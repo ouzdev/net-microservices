c5dfc36f11c885846f5a2b11386f83ee92690132
ouzdev <oguzcangencc@hotmail.com>
2021-11-29 23:56:42 +0300

    Created PlatformPublishedDto in Command Service

---
+++ CommandsService/Dtos/PlatformPublishedDto.cs
@@ -0,0 +1,10 @@
+namespace CommandService.Dtos
+{
+    public class PlatformPublishedDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Event { get; set; }
+        
+    }
+}
