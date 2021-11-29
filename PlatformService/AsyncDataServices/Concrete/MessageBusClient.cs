440e280af9787ef491536002be5a8e6f73422205
ouzdev <oguzcangencc@hotmail.com>
2021-11-29 22:55:40 +0300

    Created MessageBusClient concrete class in AsyncDataService folder

--- PlatformService/AsyncDataServices/Concrete/MessageBusClient.cs
+++ PlatformService/AsyncDataServices/Concrete/MessageBusClient.cs
@@ -1,19 +1,87 @@
 using Microsoft.Extensions.Configuration;
 using PaltformService.Dtos;
+using RabbitMQ.Client;
+using System;
+using System.Text.Json;
+using System.Text;
 
 namespace PlatformService.AsyncDataServices
 {
     public class MessageBusClient : IMessageBusClient
     {
         private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private IModel _channel;
 
         public MessageBusClient(IConfiguration configuration)
         {
             _configuration = configuration;
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"])
+            };
+
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+
+                _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+
+                _connection.ConnectionShutdown += RabbitMQ_Connection_ConnectionShutdown;
+
+                Console.WriteLine("--> Connect to MessageBus");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"--> Cannot not connect to the Message Bus: {ex.Message}");
+                throw;
+            }
         }
+
         public void PublishPlatform(PlatformPublishedDto platformPublishedDto)
         {
-                
+            var message = JsonSerializer.Serialize(platformPublishedDto);
+
+            if (_connection.IsOpen)
+            {
+                Console.WriteLine("--> RabbitMQ Connection Open, sending message");
+                SendMessage(message);
+
+            }
+            else
+            {
+                Console.WriteLine("--> RabbitMQ Conenctions closed, not sending message");
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(exchange: "trigger",
+                                    routingKey: "",
+                                    basicProperties: null,
+                                    body: body);
+            Console.WriteLine($"--> We have send {message}");
+
+
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"--> MessageBus Disposed");
+            if (_channel.IsOpen)
+            {
+                _channel.Close();
+                _connection.Close();
+            }
+        }
+
+        private void RabbitMQ_Connection_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("---> RabbitMQ Connection Shutdown");
         }
     }
 }
