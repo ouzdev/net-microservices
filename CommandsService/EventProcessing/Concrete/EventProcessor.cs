91e9e23412991d4d20a7faeda298df0ccf727b47
ouzdev <oguzcangencc@hotmail.com>
2021-11-30 00:15:33 +0300

    Created EventProcessor file in EventProcessing folder

---
+++ CommandsService/EventProcessing/Concrete/EventProcessor.cs
@@ -0,0 +1,12 @@
+using CommandService.EventProcessing;
+
+namespace CommandsService.EventProcessing
+{
+    public class EventProcessing : IEventProcessing
+    {
+        public void ProcessEvent(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
